81d94c002481a5412d0f39a980160ad1f1f71359
mcpotato <a3436370081@163.com>
2024-08-12 22:38:20 +0800

    1

---
+++ src/main/java/com/duckcity/DuckcityModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.duckcity;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DuckcityModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
